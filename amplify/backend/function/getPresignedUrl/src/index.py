98681cfe15f246b18c7f68b4eca7aa1174ba4341
Shardul Vaidya <cam.v737@gmail.com>
2022-10-08 01:06:53 -0400

    added amplify api/graphql, having some troubled deploying the system

---
+++ amplify/backend/function/getPresignedUrl/src/index.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+from botocore.exceptions import *
+
+
+"""
+Available variables in the lambda function
+    ENV
+    REGION
+"""
+
+
+def get_presigned_url(bucket_name, object_key):
+    s3_client = boto3.client('s3')
+
+    try:
+        response = s3_client.generate_presigned_url(
+            'put_object',
+            Params={
+                'Bucket': bucket_name,
+                'Key': object_key
+            },
+            ExpiresIn=3600
+        )
+
+        return json.dumps(response)
+    except ClientError as e:
+        logging.error(e)
+        return json.dumps(response)
+    pass
+
+
+def handler(event, context):
+    bucket_name = event["bucket_name"]
+    object_key = event["object_key"]
+
+    return get_presigned_url(bucket_name, object_key)
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "imageuploadfrontend": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "function": {
+        "getPresignedUrl": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
