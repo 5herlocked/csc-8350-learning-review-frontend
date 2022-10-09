9cec86d6324f50ce4e65f04f863ce469dd6ce198
Shardul Vaidya <cam.v737@gmail.com>
2022-10-09 13:04:08 -0400

    Created the function

--- amplify/backend/function/getPresignedUrl/src/index.py
+++ amplify/backend/function/getPresignedUrl/src/index.py
@@ -1,6 +1,6 @@
 import json
 import boto3
-from botocore.exceptions import *
+from botocore.exceptions import ClientError
 
 
 """
@@ -25,7 +25,7 @@ def get_presigned_url(bucket_name, object_key):
 
         return json.dumps(response)
     except ClientError as e:
-        logging.error(e)
+        print(e)
         return json.dumps(response)
     pass
 
