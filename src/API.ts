c1ba5af425f67d2c525df95d08207aed215f9ccd
Shardul Vaidya <cam.v737@gmail.com>
2022-10-09 20:00:35 -0400

    Amplify codegen configured

---
+++ src/API.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+//  This file was automatically generated and should not be edited.
+
+export type GetPresignedUrlQueryVariables = {
+  bucket_name?: string | null,
+  object_key?: string | null,
+};
+
+export type GetPresignedUrlQuery = {
+  getPresignedUrl?: string | null,
+};
---
+++ src/graphql/queries.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPresignedUrl = /* GraphQL */ `
+  query GetPresignedUrl($bucket_name: String, $object_key: String) {
+    getPresignedUrl(bucket_name: $bucket_name, object_key: $object_key)
+  }
+`;
