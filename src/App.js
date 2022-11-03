0eb0bf6de4d6f2cd43c12796a88f33940ce122d1
Shardul Vaidya <cam.v737@gmail.com>
2022-11-03 09:56:34 -0400

    Reset the app for demo day

--- src/App.js
+++ src/App.js
@@ -1,82 +1,82 @@
+import logo from './logo.svg';
 import './App.css';
 import Axios from 'axios';
 import {useState} from "react";
 import {Amplify} from "aws-amplify";
 import awsconfig from './aws-exports';
 import * as queries from './graphql/queries';
-import { API, graphqlOperation } from 'aws-amplify';
+import {API, graphqlOperation} from 'aws-amplify';
 
 Amplify.configure(awsconfig);
+
 function App() {
-  const [uploadUrl, setUploadUrl] = useState('');
-  const [fileSelectionError, setFileSelectionError] = useState("");
-  const [selectedFile, setSelectedFile] = useState(undefined);
-  const [isFilePicked, setIsFilePicked] = useState(false);
-
-  async function onFileChanged(event) {
-    console.log(event.target.files[0]);
-    const fileObj = event.target.files && event.target.files[0];
-
-    if (!fileObj) return;
-
-    const [type] = fileObj.type.split('/');
-
-    console.log(type)
-
-    if (!type || type !== 'image') {
-      setFileSelectionError("You can only upload image files");
-      return;
-    }
-
-    let uploadUrl = await API.graphql(graphqlOperation(queries.getPresignedUrl,
-        {
-          bucket_name: 's3-lambda-rekognition-source-bucket',
-          object_key: fileObj.name,
-          file_type: type,
-        }
-    ));
-
-    // console.log((uploadUrl.data)["getPresignedUrl"]);
-
-    uploadUrl = JSON.parse((uploadUrl.data)["getPresignedUrl"]).presignedUrl;
-    console.log(uploadUrl);
-
-    setUploadUrl(uploadUrl);
-    setSelectedFile(event);
-    setIsFilePicked(true);
-    setFileSelectionError("");
-  }
-
-  async function handleSubmission() {
-    const fileEvent = selectedFile.target.files && selectedFile.target.files[0];
-
-    // const formData = new FormData();
-    // formData.append('Content-Type', 'image');
-    //
-    // formData.append('file', fileEvent); // The file has to be the last element
-    //
-    // const response = await Axios.put(uploadUrl, formData, {
-    //   headers: {'Content-Type': 'multipart/form-data'},
-    // });
-
-    console.log(fileEvent);
-
-    const response = await Axios.put(
-        uploadUrl,
-        fileEvent,
-        {
-          baseURL: ''
-        }
-    );
-
-    setIsFilePicked(false);
-    setSelectedFile(undefined);
-    setUploadUrl("");
-  }
-
-  return (
-      <div className="App">
-        <header className="App-header">
+  // const [uploadUrl, setUploadUrl] = useState('');
+  // const [fileSelectionError, setFileSelectionError] = useState("");
+  // const [selectedFile, setSelectedFile] = useState(undefined);
+  // const [isFilePicked, setIsFilePicked] = useState(false);
+  //
+  // async function onFileChanged(event) {
+  //   console.log(event.target.files[0]);
+  //   const fileObj = event.target.files && event.target.files[0];
+  //
+  //   if (!fileObj) return;
+  //
+  //   const [type] = fileObj.type.split('/');
+  //
+  //   console.log(type)
+  //
+  //   if (!type || type !== 'image') {
+  //     setFileSelectionError("You can only upload image files");
+  //     return;
+  //   }
+  //
+  //   let uploadUrl = await API.graphql(graphqlOperation(queries.getPresignedUrl,
+  //       {
+  //         bucket_name: 's3-lambda-rekognition-source-bucket',
+  //         object_key: fileObj.name,
+  //         file_type: type,
+  //       }
+  //   ));
+  //
+  //   // console.log((uploadUrl.data)["getPresignedUrl"]);
+  //
+  //   uploadUrl = JSON.parse((uploadUrl.data)["getPresignedUrl"]).presignedUrl;
+  //   console.log(uploadUrl);
+  //
+  //   setUploadUrl(uploadUrl);
+  //   setSelectedFile(event);
+  //   setIsFilePicked(true);
+  //   setFileSelectionError("");
+  // }
+  //
+  // async function handleSubmission() {
+  //   const fileEvent = selectedFile.target.files && selectedFile.target.files[0];
+  //
+  // const formData = new FormData();
+  // formData.append('Content-Type', 'image');
+  //
+  // formData.append('file', fileEvent); // The file has to be the last element
+  //
+  // const response = await Axios.put(uploadUrl, formData, {
+  //   headers: {'Content-Type': 'multipart/form-data'},
+  // });
+  //
+  //   console.log(fileEvent);
+  //
+  //   const response = await Axios.put(
+  //       uploadUrl,
+  //       fileEvent,
+  //       {
+  //         baseURL: ''
+  //       }
+  //   );
+  //
+  //   setIsFilePicked(false);
+  //   setSelectedFile(undefined);
+  //   setUploadUrl("");
+  // }
+  /*
+  *
           <label>Select a file to upload</label>
           <input type="file" onChange={onFileChanged}/>
           {isFilePicked ? (
@@ -84,10 +84,27 @@ function App() {
                 <p>Filename: {selectedFile["name"]}</p>
               </div>
           ) : (
-              <p>{ fileSelectionError }</p>
+              <p>{fileSelectionError}</p>
           )
           }
-          { isFilePicked && <button  onClick={handleSubmission}>Submit</button>}
+          {isFilePicked && <button onClick={handleSubmission}>Submit</button>}
+  * */
+
+  return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
   );
